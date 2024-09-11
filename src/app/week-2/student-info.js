e84f4191bee9d78c08c03a9cc51cb201b64245d3
Alan L. <wei-cheng.lai@edu.sait.ca>
2024-09-11 13:24:40 -0600

    Week2-Assignment
    Creating Pages, Components, Links

---
+++ src/app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Student Name</h1>
+        <p>Wei-Cheng Lai</p>
+        <Link href={"https://github.com/WeiChengLai1994/cprg306-assignments"}>Github</Link>
+      </main>
+    );
+  }
